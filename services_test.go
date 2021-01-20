61d2eeda00293ecda376d850d75726f8c5dc8e58
Joern Barthel <joern.barthel@kreuzwerker.de>
2021-01-20 22:54:09 +0100

    Added tests.

---
+++ services_test.go
@@ -0,0 +1,37 @@
+package geographer
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServices(t *testing.T) {
+
+	assert := assert.New(t)
+
+	s, err := GetServices(context.TODO())
+
+	assert.NoError(err)
+
+	r := s.Services()
+
+	assert.EqualValues([]string{
+		"eu-west-1",
+		"us-east-1",
+		"us-west-2",
+	}, r["workmail"])
+
+	assert.EqualValues([]string{
+		"eu-west-1",
+		"us-east-1",
+		"us-west-2",
+	}, r["workmail"].Intersection())
+
+	assert.EqualValues([]string{
+		"eu-west-1",
+		"us-east-1",
+	}, r["workmail"].Intersection("eu-west-1", "us-east-1", "no-such-region"))
+
+}
